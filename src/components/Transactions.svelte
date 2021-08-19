2ce4fa93f6977487d9c2f62872172d1b5705fde4
0xmu <0xmu@protonmail.com>
2021-08-19 17:35:00 +0100

    Explorer link in txs

--- src/components/Transactions.svelte
+++ src/components/Transactions.svelte
@@ -1,7 +1,14 @@
 <script>
 	import { onMount } from 'svelte'
-	import { signer, address } from '../stores/provider'
+	import { signer, address, chainId } from '../stores/provider'
 	import { transactions, clearTransactions } from '../stores/transactions'
+	import { CHAIN_DATA } from '../lib/constants'
+
+	const explorer = CHAIN_DATA[$chainId]['explorer'];
+
+	function txLink(hash) {
+		return `${explorer}/tx/${hash}`; 
+	}
 
 </script>
 
@@ -12,7 +19,7 @@
 	Transactions (<a on:click={clearTransactions}>Clear</a>)
 	<ul>
 		{#each $transactions as tx}
-			<li>{tx.description} || {tx.state}</li>
+			<li>{tx.description} || {tx.state} | <a href={txLink(tx.hash)}>tx</a></li>
 		{/each}
 	</ul>
 </div>
