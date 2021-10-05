4465dec599966a90fc9f407a27000ecdddad7b9c
0xmu <0xmu@protonmail.com>
2021-10-05 19:22:48 +0100

    toasts

--- src/components/Toasts.svelte
+++ src/components/Toasts.svelte
@@ -1,82 +1,65 @@
 <script>
 	import { fade, fly } from 'svelte/transition'
 
-	import { toasts, hideToast } from '../stores/toasts'
+	import { toast, hideToast } from '../stores/toasts'
 	import { CANCEL_ICON } from '../lib/icons'
 </script>
 
 <style>
+
 	.toast-container {
 		position: fixed;
-		bottom: var(--base-padding);
-		left: var(--base-padding);
+		top: var(--base-padding);
+		left: 0;
+		right: 0;
 		z-index: 101;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 	}
-	.toast-wrapper {
-		display: grid;
-		grid-auto-flow: row;
-		grid-gap: 16px;
-		max-width: 280px;
-	}
+
 	.toast {
-		background-color: rgb(23,23,23);
-		border-radius: 8px;
+		border-radius: var(--base-radius);
 		display: flex;
 		justify-content: space-between;
-		padding: 16px;
+		padding: 12px;
+		background-color: #0D0D0D;
+		max-width: 420px;
+	}
+
+	.toast.error {
+		background-color: #290400;
+		color: #FF1900;
+		fill: #FF1900;
+	}
+	.toast.success {
+		background-color: #002901;
+		fill: var(--green);
+		color: var(--green);
 	}
 
 	.body {
 		padding-right: var(--base-padding);
-	}
-
-	.title {
+		word-break: break-word;
 		font-weight: 700;
-		text-transform: capitalize;
-		margin-bottom: 4px;
-	}
-	.text {
-		line-height: 1.45;
-		word-break: break-word
 	}
 
-	.toast.error {
-		border: 2px solid var(--red);
-		color: var(--red);
-		fill: var(--red);
-	}
-	.toast.success {
-		border: 2px solid var(--green);
-		color: var(--green);
-		fill: var(--green);
-	}
-	.toast.transaction {
-		border: 2px solid var(--blue);
-		color: var(--blue);
-		fill: var(--blue);
-	}
-
-	:global(.toast-wrapper .close svg) {
+	:global(.toast .close svg) {
 		height: 14px;
 		width:  14px;
 		margin-bottom: -2px;
 		fill: inherit;
 	}
+
 </style>
 
+{#if $toast}
 <div class='toast-container'>
-	<div class='toast-wrapper'>
-		{#each $toasts as toast}
-			<div transition:fly="{{ x: -100, duration: 250 }}" class={`toast ${toast.type || 'error'}`} data-intercept="true">
-				<div class='body'>
-					<div class='title'>{toast.type}</div>
-					<div class='text'>{toast.message}</div>
-				</div>
-				<a class='close' on:click={() => {hideToast(toast.id)}}>{@html CANCEL_ICON}</a>
-			</div>
-		{/each}
+	<div class={`toast ${$toast.type || 'error'}`} data-intercept="true">
+		<div class='body'>
+			{$toast.message}
+		</div>
+		<a class='close' on:click={() => {hideToast()}}>{@html CANCEL_ICON}</a>
 	</div>
 </div>
+{/if}
--- src/stores/toasts.js
+++ src/stores/toasts.js
@@ -1,33 +1,20 @@
 import { writable } from 'svelte/store'
 import { parseErrorToString } from '../lib/errors'
 
-export const toasts = writable([]);
+export const toast = writable({message: 'Order submitted.', type: 'success'});
 
-let lastToastId = 1;
-let timers = {};
+let timer;
 
 export function showToast(data, type) {
 	let message = parseErrorToString(data);
 	if (!type) type = 'error';
 	if (!message) return;
-	toasts.update((x) => {
-		x.unshift({message: message, type, id: lastToastId});
-		return x;
-	});
-	let _id = lastToastId;
-	timers[_id] = setTimeout(() => {hideToast(_id)}, 10*1000);
-	lastToastId++;
+	toast.set({message: message, type});
+	clearTimeout(timer);
+	timer = setTimeout(() => {hideToast()}, 10*1000);
 }
 
-export function hideToast(toastId) {
-	if (toastId) clearTimeout(timers[toastId]);
-	toasts.update((x) => {
-		let new_toasts = [];
-		for (const toast of x) {
-			if (!toastId) clearTimeout(timers[toast.id]);
-			if (toastId == toast.id) continue;
-			new_toasts.push(toast);
-		}
-		return new_toasts;
-	});
+export function hideToast() {
+	clearTimeout(timer);
+	toast.set(null);
 }
